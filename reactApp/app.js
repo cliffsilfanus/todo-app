27a861f7f8b614c88e20e72afcd810618ed9b29b
Clifford Silfanus <cliff.silfanus@gmail.com>
2019-07-18 16:20:57 -0400

    create basic todo with dummy data v2

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,7 +1,33 @@
 import React from "react";
 import { render } from "react-dom";
 
-let dummyData = ["Todo1", "Todo2", "Todo3"];
+let dummyData = [ 
+    { taskText: "This task done", completed: true }, 
+    { taskText: "This task not done", completed: false }, 
+    { taskText: "This task not done too", completed: false}
+];
+
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <InputLine />
+        <TodoList />
+      </React.Fragment>
+    );
+  }
+}
+
+class InputLine extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="task" />
+        <input type="submit" value="Add todo" />
+      </div>
+    );
+  }
+}
 
 class TodoList extends React.Component {
   render() {
@@ -23,10 +49,11 @@ class Todo extends React.Component {
   render() {
     return (
       <li>
-        <input type="submit" value="X" /> {this.props.todo}
+        <input type="submit" value="X" />
+        {this.props.todo.completed ? <strike>{this.props.todo.taskText}</strike> : this.props.todo.taskText }
       </li>
     );
   }
 }
 
-render(<TodoList />, document.getElementById("root"));
+render(<TodoApp />, document.getElementById("root"));
