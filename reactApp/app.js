0a19ae2cbedd47c096ff144c01a680b8a596f526
Clifford Silfanus <cliff.silfanus@gmail.com>
2019-07-18 15:55:25 -0400

    create basic todo with dummy data

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,4 +1,32 @@
 import React from "react";
 import { render } from "react-dom";
 
-render(<p>React lives!</p>, document.getElementById("root"));
+let dummyData = ["Todo1", "Todo2", "Todo3"];
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {dummyData.map(todo => (
+          <Todo todo={todo} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <li>
+        <input type="submit" value="X" /> {this.props.todo}
+      </li>
+    );
+  }
+}
+
+render(<TodoList />, document.getElementById("root"));
