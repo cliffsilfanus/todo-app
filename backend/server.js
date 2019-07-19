2bfb603627f8423fcd77eea65ca4cc376b8f0f85
Clifford Silfanus <cliff.silfanus@gmail.com>
2019-07-19 16:04:38 -0400

    connect server to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const dbRoutes = require("./routes/databaseAccess.js");
+
+const app = express();
 
 app.use(express.static("build"));
 app.use("/db", dbRoutes);
+
+const connect = process.env.MONGODB_URI;
+mongoose.connect(connect);
 
 app.listen(3000, () => {
   console.log("Server for React Todo App is listening on port 3000");
